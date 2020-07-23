b44d001641a794686fda512222a4e4479996a966
devbihari <amit.jha6700@gmail.com>
2020-07-23 19:46:52 +0530

    read for webinar

--- Adminapp/urls.py
+++ Adminapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("connect/", views.connect, name='questions'),
-    path('question/', views.questions, name='questions'),
-    path('start/', views.start, name='questions'),
-    path('submit/<int:qn>/<str:ans>/', views.submit, name='questions')
+    # path("connect/", views.connect, name='questions'),
+    # path('question/', views.questions, name='questions'),
+    # path('start/', views.start, name='questions'),
+    # path('submit/<int:qn>/<str:ans>/', views.submit, name='questions')
 ]
